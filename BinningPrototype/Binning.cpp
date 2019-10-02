eb7b0c067331a1f0a1029dcbd28fb93a7ae527d7
mcolina <Myles.Colina@asu.edu>
2019-10-02 18:59:32 -0400

    Working on Task#25, half complete

---
+++ BinningPrototype/Binning.cpp
@@ -0,0 +1,296 @@
+
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string.h>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+
+  static vector<Student> remainingStudents;
+  static vector<Project> project_set1;
+  static vector<Project> project_set2;
+  static vector<Project> allprojects;
+
+class Student {
+	public:
+		string name;
+		// 2D array of project # and skillscore for that project.
+
+		int projectxskill[6][2];
+
+		vector <string> negative_affinity;
+		vector <string> positive_affinity;
+		// O = online, C = on campus, N = not specified
+		string type;
+
+		Student();
+		Student(string n, vector <string> na, vector <string> pa, int pskill[6][2], string t){
+			this->name = n;
+			this->projectxskill = pskill;
+			this->negative_affinity = na;
+			this->positive_affinity = pa;
+			this->type = t;
+		}
+};
+
+class Project {
+	public:
+		string name;
+		int project_num;
+		// skillsScores rank from 0(no knowledge)-4(expert)
+		vector <string> assignedStudents;
+		// O = online, C = on campus, N = not specified
+		string type;
+		Project();
+		Project(string n, int p_num, string t){
+			this->name = n;
+			this->project_num = p_num;
+			this->type = t;
+		}
+};
+
+int randomnum()
+{
+	 srand((unsigned)time(0));
+	    int random_num;
+	    for(int index=0; index<35; index++){
+	        random_num = (rand()%10)+1;
+	      return random_num;
+	    }
+}
+
+int[6][2] randskillscore()
+{
+	int rand_matrix[6][2]=
+	{
+			{1, randomnum()},
+			{2, randomnum()},
+			{3, randomnum()},
+			{4, randomnum()},
+			{5, randomnum()},
+			{6, randomnum()},
+	};
+
+	return
+}
+//Searches over all the remaining students to find the student with the best
+//skill score for the project.
+Student find_bestStudent(int p_num)
+{  Student best_student;
+	int best_score = 0;
+	for(int i = 0; i < remainingStudents.size(); i++){
+		int score = remainingStudents[i].projectskill[p_num+1][1]
+		if (score >= best_score){
+			best_score=score;
+			best_student = remainingStudents[i];
+		}
+	}
+
+ return
+}
+
+void typecheck()
+{
+
+}
+
+void affintycheck()
+{
+
+}
+
+void preferedtimecheck()
+{
+
+}
+
+void project_team_threads()
+{
+
+}
+
+
+void project_team_assessment()
+{
+
+}
+
+
+
+int main()
+{
+
+	//vector<Student> remainingStudents;
+	//vector<Project> project_set1;
+	//vector<Project> project_set2;
+	//vector<Project> allprojects;
+
+		// Important numbers
+		int numStudents = 30;
+		int numProjects = 3;
+		int numSkills = 5;
+		int teams = numStudents / numProjects;
+
+
+
+		// Create Students, 20 Online students and 10 on-campus students
+		//The skill scores are randomly generated each time.
+		//for now, there is no affinity check.
+		vector <string> no_affinity;
+
+		Student st1("st1", no_affinity, no_affinity, randskillscore(),"O");
+		Student st2("st2",no_affinity, no_affinity, randskillscore(),"O");
+		Student st3("st3", no_affinity, no_affinity, randskillscore(),"O");
+		Student st4("st4",no_affinity, no_affinity, randskillscore(),"O");
+		Student st5("st5", no_affinity, no_affinity, randskillscore(),"O");
+		Student st6("st6",no_affinity, no_affinity, randskillscore(),"O");
+		Student st7("st7",no_affinity, no_affinity, randskillscore(),"O");
+		Student st8("st8",no_affinity, no_affinity, randskillscore(),"O");
+		Student st9("st9", no_affinity, no_affinity, randskillscore(),"O");
+		Student st10("st10", no_affinity, no_affinity, randskillscore(),"O");
+		Student st11("st11", no_affinity, no_affinity, randskillscore(),"O");
+		Student st12("st12", no_affinity, no_affinity, randskillscore(),"O");
+		Student st13("st13", no_affinity, no_affinity, randskillscore(),"O");
+		Student st14("st14", no_affinity, no_affinity, randskillscore(),"O");
+		Student st15("st15", no_affinity, no_affinity, randskillscore(),"O");
+		Student st16("st16", no_affinity, no_affinity, randskillscore(),"O");
+		Student st17("st17", no_affinity, no_affinity, randskillscore(),"O");
+		Student st18("st18", no_affinity, no_affinity, randskillscore(),"O");
+		Student st19("st19", no_affinity, no_affinity, randskillscore(),"O");
+		Student st20("st20", no_affinity, no_affinity, randskillscore(),"O");
+		Student st21("st21", no_affinity, no_affinity, randskillscore(),"C");
+		Student st23("st22", no_affinity, no_affinity, randskillscore(),"C");
+		Student st23("st23", no_affinity, no_affinity, randskillscore(),"C");
+		Student st24("st24", no_affinity, no_affinity, randskillscore(),"C");
+		Student st25("st25", no_affinity, no_affinity, randskillscore(),"C");
+		Student st26("st26", no_affinity, no_affinity, randskillscore(),"C");
+		Student st27("st27", no_affinity, no_affinity, randskillscore(),"C");
+		Student st28("st28", no_affinity, no_affinity, randskillscore(),"C");
+		Student st29("st29", no_affinity, no_affinity, randskillscore(),"C");
+		Student st30("st30", no_affinity, no_affinity, randskillscore(),"C");
+		remainingStudents.push_back(st1);
+		remainingStudents.push_back(st2);
+		remainingStudents.push_back(st3);
+		remainingStudents.push_back(st4);
+		remainingStudents.push_back(st5);
+		remainingStudents.push_back(st6);
+		remainingStudents.push_back(st7);
+		remainingStudents.push_back(st8);
+		remainingStudents.push_back(st9);
+		remainingStudents.push_back(st10);
+		remainingStudents.push_back(st11);
+		remainingStudents.push_back(st12);
+		remainingStudents.push_back(st13);
+		remainingStudents.push_back(st14);
+		remainingStudents.push_back(st15);
+		remainingStudents.push_back(st16);
+		remainingStudents.push_back(st17);
+		remainingStudents.push_back(st18);
+		remainingStudents.push_back(st19);
+		remainingStudents.push_back(st20);
+		remainingStudents.push_back(st21);
+		remainingStudents.push_back(st22);
+		remainingStudents.push_back(st23);
+		remainingStudents.push_back(st24);
+		remainingStudents.push_back(st25);
+		remainingStudents.push_back(st26);
+		remainingStudents.push_back(st27);
+		remainingStudents.push_back(st28);
+		remainingStudents.push_back(st29);
+		remainingStudents.push_back(st30);
+
+
+		// Create Projects Set1 (highest priority projects)
+		Project p1("p1", 1, "O");
+		Project p2("p2", 2, "N");
+		Project p3("p3", 3, "C");
+		projects_set1.push_back(p1);
+		projects_set1.push_back(p2);
+		projects_set1.push_back(p3);
+
+		// Create Projects Set2 (lowest priority projects)
+		Project p4("p1", 1, "N");
+		Project p5("p2", 2, "N");
+		Project p6("p3", 3, "O");
+		projects_set2.push_back(p4);
+		projects_set2.push_back(p5);
+		projects_set2.push_back(p6);
+
+
+		allprojects.push_back(p1);
+		allprojects.push_back(p2);
+		allprojects.push_back(p3);
+		allprojects.push_back(p4);
+		allprojects.push_back(p5);
+		allprojects.push_back(p6);
+
+
+		// Assign Students to Projects
+
+		// assignedStudents will keep track of students that have already been selected for a project.
+		// each element is a student name (ie for st12, it would be 12)
+		vector<int> assignedStudents;
+
+		// firstStudent, secondStudent, thirdStudent, and fourthStudent will be Student name.
+		// 1 will be for st1, 12 for st12
+		// first, second, third, and fourth will store the values of the 4 highest scoring students per project.
+		// Students already selected for a previous project will be omitted.
+		int firstStudent, secondStudent, thirdStudent, fourthStudent;
+		int first, second, third, fourth;
+
+		// For each project
+		for (int i = 0; i < numProjects; i++){
+			firstStudent = secondStudent = thirdStudent = fourthStudent = first = second = third = fourth = -1;
+			// For each Student
+			for (int j = 0 ; j < numStudents; j++){
+				if (contains(assignedStudents, j+1) == true){
+					continue;
+				}
+				if (projectxstudent[i][j] > first){
+					fourth = third;
+					fourthStudent = thirdStudent;
+					third = second;
+					thirdStudent = secondStudent;
+					second = first;
+					secondStudent = firstStudent;
+					first = projectxstudent[i][j];
+					firstStudent = j+1;
+				}
+				else if (projectxstudent[i][j] > second){
+					fourth = third;
+					fourthStudent = thirdStudent;
+					third = second;
+					thirdStudent = secondStudent;
+					second = projectxstudent[i][j];
+					secondStudent = j+1;
+				}
+				else if (projectxstudent[i][j] > third){
+					fourth = third;
+					fourthStudent = thirdStudent;
+					third = projectxstudent[i][j];
+					thirdStudent = j+1;
+				}
+				else if (projectxstudent[i][j] > fourth){
+					fourth = projectxstudent[i][j];
+					fourthStudent = j+1;
+				}
+			} // end student loop
+			assignedStudents.push_back(firstStudent);
+			assignedStudents.push_back(secondStudent);
+			assignedStudents.push_back(thirdStudent);
+			assignedStudents.push_back(fourthStudent);
+			cout << "p" + to_string(i+1) + " team members" << endl;
+			cout << "1) st" + to_string(firstStudent) + "\t" + to_string(first) << endl;
+			cout << "2) st" + to_string(secondStudent) + "\t" + to_string(second) << endl;
+			cout << "3) st" + to_string(thirdStudent) + "\t" + to_string(third) << endl;
+			cout << "4) st" + to_string(fourthStudent) + "\t" + to_string(fourth) << endl;
+			cout << endl;
+		}//end project loop
+
+		return 0;
+	}
+
