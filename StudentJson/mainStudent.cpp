668dd5b2ce7885fe79aa00bfa01d4b5455454e9d
mcolina <Myles.Colina@asu.edu>
2019-10-06 20:32:41 -0400

    Task#39-created the folder and main file readr class

---
+++ StudentJson/mainStudent.cpp
@@ -0,0 +1,33 @@
+/*
+ * 	mainStudent.cpp
+ *
+ *  Created on: Oct. 6, 2019
+ *      Author: Myles Colina
+ *      Github username: mcolina
+ *
+ *      Description:
+ *      A main class to read in student data from a Json file.
+ *
+ */
+
+#include <iostream>
+#include "json/json.h"
+
+//#include "student.h"
+
+using namespace std;
+
+
+int main(){
+
+	//load in the data from a Json file
+	StudentList student_list;
+	student_list.JsonLoad("nameofstudentfile.json");
+
+
+
+	//not really needed
+	//write the data to a json file
+	student_list.JsonSave("studentlist.json");
+
+}
