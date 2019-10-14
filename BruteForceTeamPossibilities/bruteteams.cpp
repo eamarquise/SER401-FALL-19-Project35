05cf9dffb170b865df3db6acc7fa810f135abdaa
MattCilibraise <mcilibra@asu.edu>
2019-10-14 19:13:43 -0400

    Update -
    implemented algorithm to print all unique combinations of M size in a
    set of N integers (simulated student names)

--- BruteForceTeamPossibilities/bruteteams.cpp
+++ BruteForceTeamPossibilities/bruteteams.cpp
@@ -12,6 +12,8 @@
 #include <fstream>
 #include <algorithm>
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include <string.h>
 #include <fstream>
@@ -25,7 +27,58 @@
 #include <math.h>
 
 using namespace std;
+int possiblities = 0;
 
-int main(){
+// A function to print all combination of a given length from the given array.
+void getCombinations(int a[], int sizeofteam, int startIndex, int currentTeamSize, bool assignedStudents[], int totalNumStudents)
+{
+	// Return if the currLen is more than the required length.
+	if(currentTeamSize > sizeofteam)
+	return;
+	// If currLen is equal to required length then print the sequence.
+	else if (currentTeamSize == sizeofteam)
+	{
+		cout<<"\t";
+		for (int i = 0; i < totalNumStudents; i++)
+		{
+			if (assignedStudents[i] == true)
+			{
+				cout<<a[i]<<" ";
+				possiblities++;
+			}
+		}
+		cout<<"\n";
+		return;
+	}
+	// If startIndex equals to totalNumStudents then return. No elements left.
+	if (startIndex == totalNumStudents)
+	{
+		return;
+	}
+
+	// If we select the student, put true into assignedStudents, increment our currentTeamSize and startIndex.
+	assignedStudents[startIndex] = true;
+	getCombinations(a, sizeofteam, startIndex + 1, currentTeamSize + 1, assignedStudents, totalNumStudents);
+	// If we dont select the student, put false in the assignedStudents and increment our startIndex.
+	assignedStudents[startIndex] = false;
+	getCombinations(a, sizeofteam, startIndex + 1, currentTeamSize, assignedStudents, totalNumStudents);
+}
+
+int main()
+{
+	int teamSize = 5;
+	int numStudents = 50;
+
+	bool assignedStudents[numStudents];
+
+	int studentList[numStudents] = {0};
+	for (int i = 0; i < numStudents; i++){
+		assignedStudents[i]= false;
+		studentList[i] = i;
+	}
+
+	getCombinations(studentList, teamSize, 0, 0, assignedStudents, numStudents);
+	cout << endl << "possiblities: " << possiblities;
+
 	return 0;
 }
