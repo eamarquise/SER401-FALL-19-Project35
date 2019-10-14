cac5c30b477bf7c4ac5c500be4bc5a6599f4cd10
MattCilibraise <mcilibra@asu.edu>
2019-10-14 17:21:17 -0400

    Setting up project

---
+++ BruteForceTeamPossibilities/Student.h
@@ -0,0 +1,37 @@
+/*
+ * Student.cpp
+ * Description:
+ * 		A Class to describe students & related
+ *
+ *  Created on: Sep. 30, 2019
+ *      Author: mcilibra
+ *
+ *   Edited on: Oct. 06, 2019
+ *      Author: eamarquise
+ */
+
+#ifndef STUDENT_H_
+#define STUDENT_H_
+#include <string.h>
+#include "Skills.h"
+#include "PreferredMeetingTimes.h"
+#include "Affinity.h"
+
+class Student {
+	public:
+
+		//Variables
+		std::string name;	//Student name
+		Skills studentSkills;	//Student skills
+		PreferredMeetingTimes timesAvailable;	//Meetingtimes
+		Affinity affinity;	//Work with someone or not
+		bool online;	//Online student:  0(false)-local student, 1(true)-online student
+
+		Student();
+		Student(std::string n, Skills s, PreferredMeetingTimes times, Affinity aff, bool online);
+		bool operator==(const Student &studentToCompare) const;
+		bool operator==(const Student *studentToCompare) const;
+
+};
+
+#endif /* STUDENT_H_ */
---
+++ BruteForceTeamPossibilities/bruteteams.cpp
@@ -0,0 +1,31 @@
+/*
+ * bruteteams.cpp
+ *
+ *  Created on: Oct. 14, 2019
+ *      Author: mcilibra
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <fstream>
+#include <algorithm>
+#include <time.h>
+
+#include <string.h>
+#include <fstream>
+#include <algorithm>
+#include <time.h>
+#include "Skills.h"
+#include "Affinity.h"
+#include "PreferredMeetingTimes.h"
+#include "Student.h"
+#include "Project.h"
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	return 0;
+}
