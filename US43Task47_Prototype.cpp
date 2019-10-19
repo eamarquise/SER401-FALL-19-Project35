83696be2d8913d42c81847849d45a7973d1e0d03
Cristi DeLeo <cristi.deleo@asu.edu>
2019-10-19 00:45:20 -0400

    [10/19/2019 Cristi DeLeo] Working partition algorithm for multi-dim ProjectPool matrix.

--- US43Task47_Prototype.cpp
+++ US43Task47_Prototype.cpp
@@ -5,21 +5,15 @@
 #include <forward_list>
 #include <ctime>
 #include <chrono>
+#include <set>
 
 using namespace std::chrono;
-
-/*
- *
- *
- *
- *
- */
 
 template <class ForwardIt>
     void quicksort(ForwardIt first, ForwardIt last)
     {
         if(first==last) return;
-        auto pivot = *next(first, distance(first,last)/2);
+        auto pivot = *std::next(first, std::distance(first,last)/2);
         ForwardIt middle1 = std::partition(first, last, [pivot](const auto& em){return em < pivot;});
         ForwardIt middle2 = std::partition(middle1, last,
                             [pivot](const auto& em){return !(pivot < em);});
@@ -27,36 +21,88 @@ template <class ForwardIt>
         quicksort(middle2, last);
 }
 
+struct Project {
+    int id;
+    int sponsorID;
+    int type; // 0 = Online | 1 = Ground | 2 = Hybrid
+    int priority; // 0 = Not Required | 1 = Required
+    int classID;
+    int skills();
+    std::string teamMembers();
+    int compatibilityRating;
+};
+
+int getRandomInt(int min, int max){
+	int value = rand() % (max-min +1) + min;
+	return value;
+}
+
+int getUniqueInt(int min, int max){
+    static std::vector<int> generatedValues;
+    int value = getRandomInt(min, max);
+    while (std::find(generatedValues.begin(), generatedValues.end(), value) != generatedValues.end()) {
+        value = getRandomInt(min, max);
+    }
+    return value;
+}
+
 int main()
 {
     auto start = high_resolution_clock::now();
 
-    int NUM_ROWS = 100;
-    int NUM_COLS = 5;
+    int MIN_PROJECTS = 1;
+    int MAX_PROJECTS = 100;
+    int NUM_PROJECT_ATTRIBUTES = 5;
+    int NUM_SKILLS = 7;
+    int NUM_STUDENTS = 500;
+    int TEAM_SIZE = 5;
 
-    int* A = new int[NUM_ROWS * NUM_COLS];
+    std::vector<Project> projectPool(MAX_PROJECTS);
+    std::vector<int> projectTypeArray(MAX_PROJECTS);
+    Project project;
 
-    for (int i = 0; i < NUM_ROWS; i++)
-        for (int j = 0; j < NUM_COLS; j++)
-            *(A + i*NUM_COLS + j) = rand() % NUM_ROWS;
-
-
-    for (int i = 0; i < NUM_ROWS; i++)
+    for (int i = 0; i < MAX_PROJECTS; i++)
     {
-        for (int j = 0; j < NUM_COLS; j++)
-            std::cout << *(A + i*NUM_COLS + j) << " ";
+        project.id = MIN_PROJECTS + i;
+        project.sponsorID = getUniqueInt(1, MAX_PROJECTS);
+        project.type = getRandomInt(0, 2);
+        project.priority = getRandomInt(0, 1);
+
+        projectPool[i] = project;
+        projectTypeArray[i] = project.type;
+    }
+
+    std::cout << "Project ID" << " ";
+    std::cout << "Sponsor ID" << " ";
+    std::cout << "Type" << " ";
+    std::cout << "Priority" << " ";
+    std::cout << std::endl;
+
+    for (int i = 0; i < MAX_PROJECTS; i++)
+    {
+        std::cout << projectPool[i].id << " ";
+        std::cout << projectPool[i].sponsorID << " ";
+        std::cout << projectPool[i].type << " ";
+        std::cout << projectPool[i].priority << " ";
         std::cout << std::endl;
     }
 
-    std::vector<int> *ptr = *A;
+    auto it = std::partition(projectPool.begin(), projectPool.end(), [](Project p){
+        return p.type == 0;
+    });
 
-    auto it = std::partition(ptr[2], ptr[498], [](int i){return i % 2 == 0;});
 
-    copy(*(A + 4), it, std::ostream_iterator<int>(std::cout, " "));
-    std::cout << " * " ;
-    copy(it, *(A + 103), std::ostream_iterator<int>(std::cout, " "));
 
-    delete[]A;
+    std::cout << "Begin partitioned Project Pool:" << std::endl;
+
+    for (int i = 0; i < MAX_PROJECTS; i++)
+    {
+        std::cout << projectPool[i].id << " ";
+        std::cout << projectPool[i].sponsorID << " ";
+        std::cout << projectPool[i].type << " ";
+        std::cout << projectPool[i].priority << " ";
+        std::cout << std::endl;
+    }
 
     return 0;
 }
