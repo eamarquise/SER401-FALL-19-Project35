6fb2055ad533e63fe554fcc5cf2368ad6d6df1ac
mcolina <Myles.Colina@asu.edu>
2019-10-22 12:41:38 -0400

    Completed Task#63 Prototype for Project to student comparisons

--- US#45PrototypeA/PskillCompare.cpp
+++ US#45PrototypeA/PskillCompare.cpp
@@ -48,13 +48,20 @@ class Student {
 //Student Team class
 class Team {
 	public:
-	
+	        string name;
 		vector <Student> team;
-		int teamScore;
-		Team(vector <Student> t){
-		this->team = t;		
-		teamScore=0;		}
+		vector<int> teamScores;
+		Team(string n, vector <Student> t){
+		this->name = n;
+		this->team = t;			
+		vector<int> v(numProjects, 0);
+                this->teamScores=v;	}
+
+bool operator==(const Team &rhs) const {
+		        return rhs.name == name;		    }
 };
+
+
 
 
 class Project {
@@ -152,12 +159,72 @@ for(int k = 0; k < p[i].skills.size(); k++){
 
 }}
 
-st[j].skillScores[p[i].pnum-1] = score;
+st[j].skillScores[i] = score;
 score=0;}
 }
 }
 
 }
+
+//Project skills to Team skills comparision function.
+// It evalutes a list of teams, and sorts them based
+// on how closly thier skills match the max skills 
+// of a given project .
+vector <Team> sortT(Project p, vector <Team> t)
+{
+
+vector <Team> teamsSorted;
+vector <Team> teams = t;
+
+int best=0;
+vector <Student> temp;
+Team tempT("T1", temp);
+
+
+while(teams.size() != 0){
+for(int i = 0; i < teams.size(); i++){
+
+if (best<=teams[i].teamScores[p.pnum-1]){
+  best = teams[i].teamScores[p.pnum-1];
+  tempT = teams[i];
+}
+
+}
+
+teamsSorted.push_back(tempT);
+int pos = find(teams.begin(), teams.end(), tempT) - teams.begin();
+	       teams.erase(teams.begin() + pos);
+best = 0;
+}
+
+
+return teamsSorted;
+
+}
+
+//Project skills to Team skills comparision function.
+// It evalutes all students skill scores for all projects.
+void PTeamCompare(vector <Project>& p, vector <Team>& t)
+{
+int score=0;
+
+for(int i = 0; i < 4; i++){
+
+for(int j = 0; j < 3; j++){
+
+for(int k = 0; k < 5; k++){
+//score +=5;
+score += t[i].team[k].skillScores[j];
+}
+t[i].teamScores[j] = score;
+score=0;
+}
+
+}
+return;
+}
+
+
 
 
 int main()
@@ -257,7 +324,7 @@ int main()
 
 
 
-	// calculte the skills scores for each student
+	// calculate the skills scores for each student
 	// for each of the projects.
 	PskillsCompare(allProjects, allStudents);
 	
@@ -277,7 +344,7 @@ int main()
 
 
 
-	//Student to student comparisions
+	//Project to student comparisions
 		cout << "PROJECT TO STUDENT COMPARISIONS" << endl;
 		cout << "==================================================" << endl;
 		cout << endl;		
@@ -307,6 +374,100 @@ int main()
 		cout << "Project #3 Max skill score: " + to_string(p3.maxScore)<< endl;
 		for(int i = 0; i < p3Students.size(); i++){
 		cout <<  p3Students[i].name + " score: " + to_string(p3Students[i].skillScores[2]) << endl;
+		}
+		cout << endl;	
+		cout << endl;
+
+
+	//Project to Teams comparisions
+		cout << "PROJECT TO TEAM COMPARISIONS" << endl;
+		cout << "==================================================" << endl;
+		cout << endl;
+
+
+                vector <Team> allTeams;
+		//creating a team of 5 star students
+		vector <Student> Team1;	
+		Team1.push_back(allStudents[0]);
+		Team1.push_back(allStudents[1]);
+		Team1.push_back(allStudents[4]);
+		Team1.push_back(allStudents[7]);
+		Team1.push_back(allStudents[10]);
+
+		Team T1("T1", Team1);
+
+		//creating a team of 5 middle performer students
+		vector <Student> Team2;	
+		Team2.push_back(allStudents[0]);
+		Team2.push_back(allStudents[4]);
+		Team2.push_back(allStudents[8]);
+		Team2.push_back(allStudents[11]);
+		Team2.push_back(allStudents[12]);
+
+		Team T2("T2", Team2);
+
+		//creating a team of 5 'dud' students
+		vector <Student> Team3;	
+		Team3.push_back(allStudents[5]);
+		Team3.push_back(allStudents[6]);
+		Team3.push_back(allStudents[9]);
+		Team3.push_back(allStudents[13]);
+		Team3.push_back(allStudents[14]);
+
+		Team T3("T3", Team3);
+
+		//creating a mixed team students
+		// 2 star, 2 dud, 1 middle preformer
+		vector <Student> Team4;	
+		Team4.push_back(allStudents[0]);
+		Team4.push_back(allStudents[7]);
+		Team4.push_back(allStudents[13]);
+		Team4.push_back(allStudents[14]);
+		Team4.push_back(allStudents[8]);
+
+		Team T4("T4", Team4);
+
+		allTeams.push_back(T1);
+		allTeams.push_back(T2);
+		allTeams.push_back(T3);
+		allTeams.push_back(T4);
+
+
+		PTeamCompare(allProjects, allTeams);
+
+  	// Get a sorted team list for each project based
+	//on the team skill scores.
+	 vector <Team> p1Teams;
+ 	 p1Teams = sortT(p1, allTeams);
+
+	 vector <Team> p2Teams;
+ 	 p2Teams = sortT(p2, allTeams);
+
+	 vector <Team> p3Teams;
+ 	 p3Teams = sortT(p3, allTeams);
+
+ //print out results to console.
+		cout << "Project #1 Max team score: " + to_string(p1.maxScore * 5)<< endl;
+	 	cout << "Sorted team list for projet #1 "<< endl;
+		for(int i = 0; i < p1Teams.size(); i++){
+		cout <<  p1Teams[i].name + " score: " + to_string(p1Teams[i].teamScores[0]) << endl;
+		}
+		cout << endl;	
+		cout << endl;
+
+		cout << "Project #2 Max team score: " + to_string(p2.maxScore * 5)<< endl;
+	 	cout << "Sorted team list for projet #2 "<< endl;
+		for(int i = 0; i < p2Teams.size(); i++){
+		cout <<  p2Teams[i].name + " score: " + to_string(p2Teams[i].teamScores[1]) << endl;
+		}
+		cout << endl;	
+		cout << endl;
+
+
+		cout << "Project #3 Max team score: " + to_string(p3.maxScore * 5)<< endl;
+	 	cout << "Sorted team list for projet #3 "<< endl;
+		for(int i = 0; i < p3Teams.size(); i++){
+		cout <<  p3Teams[i].name + " score: " + to_string(p3Teams[i].teamScores[2]) << endl;
 		}
 		cout << endl;	
 		cout << endl;
