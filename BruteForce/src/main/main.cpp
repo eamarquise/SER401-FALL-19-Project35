fe80ac35f708eca0b9930ccb7939a3605d769a14
mcolina <Myles.Colina@asu.edu>
2019-10-30 17:42:09 -0400

    Working on Task#114 Project to class section percentage comparison function

--- BruteForce/src/main/main.cpp
+++ BruteForce/src/main/main.cpp
@@ -56,6 +56,12 @@ int main(){
 	    cout<<"MAIN.CPP TEST"<<endl;
 	  	cout<< "Number of projects read in: " + to_string(PJson.allProjects.size())<<endl;
 
+	  	int percent= 0;
+	  		  				int x = 0;
+	  		  				x = (15 * 3);
+	  		  				percent = 35/ x;
+	  		  				percent = percent * 100;
+	  		  				cout << percent<<endl;
 
 	// Drivers to read in rules, like class section definitions
 	// ex - getRules(capStoneCourseDefinitions);
@@ -73,5 +79,80 @@ int main(){
 }
 
 
+int NumOfClassSections = 4;
+
+ int** ClassSectionSkills(vector<vector<Student>> studentList, vector<Project> projectList){
+
+	 int SectionSkills[NumOfClassSections][7] = {0};
+
+	 for(int i = 0; i < NumOfClassSections; i++){
+
+	 //number of students in class sections
+	 int numStudents = studentList[i].size();
+
+	 	 for(int j = 0; j < numStudents; j++){
+
+	 		 for(int k = 0; k < 7; k++){
+	 			 SectionSkills[i][k] += studentList[i][j].Skills[k];
+	 }}}
+
+	 int numProjects = projectList.size();
+	  	// create skillXproject matrix
+	  	int skillXproject[numProjects][7] = {0};
+	  	for (int i = 0; i < 7; i++){
+	  		for (int j = 0; j < numProjects; j++){
+	  			skillXproject[i][j] = projectList[i].Skills[j];
+	  		}
+	  	}
+
+	  	   // Calculate Project x Section skills Matrix
+	  		int projectXsection[numProjects][NumOfClassSections];
+	  		for (int rows = 0; rows < NumOfClassSections; rows++){
+
+	  			// do the multiplication
+	  			for (int cols = 0; cols < numProjects; cols++){
+	  				projectXsection[rows][cols] = {0};
+	  				for (int inner = 0; inner < 7; inner++){
+	  					projectXsection[rows][cols] = projectXsection[rows][cols] +  SectionSkills[rows][inner] * skillXproject[inner][cols];
+
+	  				}
+	  				cout << projectXsection[rows][cols] << "\t";
+	  			}
+	  			cout << "\n";
+	  		}
+	  		cout << endl;
+
+
+
+	  		int ProjectSkills[numProjects][1] = {0};
+	  			  	for (int i = 0; i < numProjects; i++){
+	  			  		int sum = 0;
+	  			  		for (int j = 0; j < 7; j++){
+	  			  		  sum  = sum + projectList[i].Skills[j];
+	  			  		}
+	  			  	   ProjectSkills[i][0] = sum * 4;
+	  			  	}
+
+//Figure out how to correctly calculate percentages.
+	  		int percentMatrix[numProjects][NumOfClassSections] = {0};
+
+	  		for (int i = 0; i < numProjects; i++){
+
+	  			 for (int j = 0; j < NumOfClassSections; j++){
+	  				int percent= 0;
+	  				int x = 0;
+	  				x = (ProjectSkills[i][0] * studentList[j].size());
+	  				percent = projectXsection[j][i]/ x;
+	  				percent = percent * 100;
+
+	  			  	 percentMatrix[i][j] = percent ;
+
+	  			 }}
+
+
+
+
+	  		return  percentMatrix;
+ }
 
 
