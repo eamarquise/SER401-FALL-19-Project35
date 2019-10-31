01fd81800d68df348db23569b376af4ed6808d3f
Sean M Rogers <seanmikerog@gmail.com>
2019-10-30 19:39:48 -0700

    Change algorithm B to use structs
    Next is to get use a json file for project and student data

---
+++ BruteForce/src/main/StudentsToProjects.h
@@ -0,0 +1,18 @@
+/*
+ * StudentsToProjects.h
+ *
+ *  Created on: Oct 30, 2019
+ *      Author: sean
+ */
+
+#ifndef BRUTEFORCE_SRC_MAIN_STUDENTSTOPROJECTS_H_
+#define BRUTEFORCE_SRC_MAIN_STUDENTSTOPROJECTS_H_
+
+class StudentsToProjects {
+public:
+
+	StudentsToProjects();
+	virtual ~StudentsToProjects();
+};
+
+#endif /* BRUTEFORCE_SRC_MAIN_STUDENTSTOPROJECTS_H_ */
