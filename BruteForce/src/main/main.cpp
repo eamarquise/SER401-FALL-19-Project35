f416198922598606333ff23737851436fcfda35c
Sean <seanmikerog@gmail.com>
2020-01-17 16:47:04 -0700

    fltk test

--- BruteForce/src/main/main.cpp
+++ BruteForce/src/main/main.cpp
@@ -60,6 +60,10 @@
 #include "sys/types.h"
 #include "sys/sysinfo.h"
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
 using namespace std;
 using namespace std::chrono;
 
@@ -516,6 +520,17 @@ int main(){
 		srand(time(NULL));
 
 	cout << "Hi Team 35" << endl;
+
+	  Fl_Window *window = new Fl_Window(340,180);
+	  Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, Worldsssss!");
+	  box->box(FL_UP_BOX);
+	  box->labelfont(FL_BOLD+FL_ITALIC);
+	  box->labelsize(36);
+	  box->labeltype(FL_SHADOW_LABEL);
+	  window->show();
+	  window->end();
+	  Fl::run();
+
 
 	//reading in inputs
 	int tempProj, tempStud;
