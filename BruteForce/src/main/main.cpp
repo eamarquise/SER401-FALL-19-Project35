8ebd588ae4de3c85219fd5ed0d4938395d6a9b61
mcolina <Myles.Colina@asu.edu>
2020-01-21 16:14:33 -0500

    FLTK update

--- BruteForce/src/main/main.cpp
+++ BruteForce/src/main/main.cpp
@@ -60,6 +60,9 @@
 #include "sys/types.h"
 #include "sys/sysinfo.h"
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 
 
 using namespace std;
@@ -517,7 +520,21 @@ int main(){
 	    auto start = high_resolution_clock::now();
 		srand(time(NULL));
 
-	cout << "Hi Team 35" << endl;
+	cout << "Hi Team 35!" << endl;
+
+/*
+  Fl_Window *window = new Fl_Window(300,180);
+  Fl_Box *box = new Fl_Box(20,40,260,100,"Hello, World!");
+  box->box(FL_UP_BOX);
+  box->labelsize(36);
+  box->labelfont(FL_BOLD+FL_ITALIC);
+  box->labeltype(FL_SHADOW_LABEL);
+  window->end();
+  window->show();
+  Fl::run();
+*/
+
+
 
 	//reading in inputs
 	int tempProj, tempStud;
