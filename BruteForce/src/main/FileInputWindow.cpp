a67ad100d62f1921dcad5bc70039f602cc76c77d
MattCilibraise <mcilibra@asu.edu>
2020-01-28 18:25:21 -0500

    Task 198 quick update

--- BruteForce/src/main/FileInputWindow.cpp
+++ BruteForce/src/main/FileInputWindow.cpp
@@ -14,7 +14,7 @@ FileInputWindow::FileInputWindow() {
 
 	Fl_Window *window = new Fl_Window(1000,500,"Capstone Team Assignment File Selection" );
 	Fl_Box *labelIntroduction = new Fl_Box(100,50,300,20,"Student File");
-	Fl_File_Input *fileInput_Student = new Fl_File_Input(100,75,300,30);
+	Fl_File_Input *fileInput_Student = new Fl_File_Input(100,75,300,35);
 
 	window->begin();
 	labelIntroduction->box(FL_FRAME_BOX);
