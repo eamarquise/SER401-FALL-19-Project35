78280194282c57c31b915f6e2d21c7b160ce1033
MattCilibraise <mcilibra@asu.edu>
2020-01-28 17:30:40 -0500

    Created FileInputWindow Class

---
+++ BruteForce/src/main/FileInputWindow.cpp
@@ -0,0 +1,21 @@
+/*
+ * FileInputWindow.cpp
+ *
+ *  Created on: Jan. 28, 2020
+ *      Author: mcilibra
+ */
+
+#include "FileInputWindow.h"
+
+namespace std {
+
+FileInputWindow::FileInputWindow() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FileInputWindow::~FileInputWindow() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ BruteForce/src/main/FileInputWindow.h
@@ -0,0 +1,21 @@
+/*
+ * FileInputWindow.h
+ *
+ *  Created on: Jan. 28, 2020
+ *      Author: mcilibra
+ */
+
+#ifndef SRC_MAIN_FILEINPUTWINDOW_H_
+#define SRC_MAIN_FILEINPUTWINDOW_H_
+
+namespace std {
+
+class FileInputWindow {
+public:
+	FileInputWindow();
+	virtual ~FileInputWindow();
+};
+
+} /* namespace std */
+
+#endif /* SRC_MAIN_FILEINPUTWINDOW_H_ */
