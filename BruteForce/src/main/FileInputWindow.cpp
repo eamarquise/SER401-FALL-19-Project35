b437cf6b41844e5e541e195dc56fb8a97899630c
MattCilibraise <mcilibra@asu.edu>
2020-01-28 18:36:06 -0500

    Task 199 added file input window for project file

--- BruteForce/src/main/FileInputWindow.cpp
+++ BruteForce/src/main/FileInputWindow.cpp
@@ -13,12 +13,16 @@ FileInputWindow::FileInputWindow() {
 
 
 	Fl_Window *window = new Fl_Window(1000,500,"Capstone Team Assignment File Selection" );
-	Fl_Box *labelIntroduction = new Fl_Box(100,50,300,20,"Student File");
-	Fl_File_Input *fileInput_Student = new Fl_File_Input(100,75,300,35);
+	Fl_Box *labelStudentFile = new Fl_Box(100,50,300,20,"Student File");
+	Fl_File_Input *fileInput_Student = new Fl_File_Input(100,71,300,35);
+	Fl_Box *labelProjectFile = new Fl_Box(100,150,300,20,"Project File");
+	Fl_File_Input *fileInput_Project = new Fl_File_Input(100,171,300,35);
 
 	window->begin();
-	labelIntroduction->box(FL_FRAME_BOX);
-	labelIntroduction->color(FL_WHITE);
+	labelStudentFile->box(FL_FRAME_BOX);
+	labelStudentFile->color(FL_WHITE);
+	labelProjectFile->box(FL_FRAME_BOX);
+	labelProjectFile->color(FL_WHITE);
 	window->end();
 	window->show();
 	Fl::run();
