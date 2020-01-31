984ca9e0627ee5db52e5c1f003df815b6ddceae3
mcolina <Myles.Colina@asu.edu>
2020-01-31 10:21:44 -0500

    Completed Task#243 Updated Class section attributes and Json files/readers

--- BruteForce/src/main/ClassSection.h
+++ BruteForce/src/main/ClassSection.h
@@ -18,8 +18,15 @@
 #ifndef CLASSSECTION_H_
 #define CLASSSECTION_H_
 
+#include <string>
+
+using namespace std;
+
 struct ClassSection {
-	int ClassID;	// Unique integer that identifies each unique instance of a ClassSection.
+	int OfficialClassID; // Unique 5 digit integer that identifies each unique instance of a ClassSection.
+	int ClassID;	// Tokenized form of the classID (numbered 0,1,2,3etc depending on the number of class sections).
+	string Course_Name; //Name of the Course,is not unique for multiple offering in the same semester.
+	string Course_Code; //Unique string for the Course of the format (YYYYSemester-X-PREFIXNUMBER-5digitID)
 	char Type;  	// Some ClassSections are dedicated to online or local students to the univerisity. Type is defined as: O = online | G = ground
 	int Enrollment;	// Common integer depicting the number of students enrolled in the instance of a ClassSection.
 
