b9227cc48c39a4b919f08010e7553794028c1edd
MattCilibraise <mcilibra@asu.edu>
2020-02-01 12:53:34 -0500

    Task #200 and #201
    Added native file chooser for student and project file. Callbacks insert
    file name into respective inputs.
    
    need to research:
    Gtk-Message: 12:49:55.231: Failed to load module "canberra-gtk-module"
    
    Next need to create a back and next button. back button needs to call
    previous window. May need to update how student file is loaded. Maybe
    this window will eventually only be used to get the project file because
    the student file may be loaded directly from canvas.
    
    Next button will need to do something with the values in the FL_Inputs.

--- BruteForce/src/main/FileInputWindow.cpp
+++ BruteForce/src/main/FileInputWindow.cpp
@@ -5,36 +5,70 @@
  *      Author: mcilibra
  */
 
+
 #include "FileInputWindow.h"
 
 namespace std {
-char filename[256]="";
-int changedValue = 0;
 
-void beepcb(Fl_Widget *, void *){
-	fl_beep();
-	fflush(stdout);
+Fl_Input *fileInput_Student = NULL;
+Fl_Input *fileInput_Project = NULL;
+
+void chooseStudentFile_cb(Fl_Widget*, void*){
+	Fl_Native_File_Chooser fileChooser;
+	fileChooser.title("Choose File");
+	fileChooser.type(Fl_Native_File_Chooser::BROWSE_FILE);
+	fileChooser.preset_file(fileInput_Student->value());
+	switch ( fileChooser.show() ) {
+		default:
+			if ( fileChooser.filename() ) {
+				fileInput_Student->value(fileChooser.filename());
+			} else {
+				fileInput_Student->value("NULL");
+			}
+			break;
+	}
 }
 
+void chooseProjectFile_cb(Fl_Widget*, void*){
+	Fl_Native_File_Chooser fileChooser;
+	fileChooser.title("Choose File");
+	fileChooser.type(Fl_Native_File_Chooser::BROWSE_FILE);
+	fileChooser.preset_file(fileInput_Project->value());
+	switch ( fileChooser.show() ) {
+		default:
+			if ( fileChooser.filename() ) {
+				fileInput_Project->value(fileChooser.filename());
+			} else {
+				fileInput_Project->value("NULL");
+			}
+			break;
+	}
+}
 
 FileInputWindow::FileInputWindow() {
 
-
 	Fl_Window *window = new Fl_Window(500,500,"Capstone Team Assignment File Selection" );
-	Fl_Box *labelStudentFile = new Fl_Box(50,50,300,20,"Student File");
-	Fl_File_Input *fileInput_Student = new Fl_File_Input(50,70,300,35);
-	Fl_Box *labelProjectFile = new Fl_Box(50,150,300,20,"Project File");
-	Fl_File_Input *fileInput_Project = new Fl_File_Input(50,170,300,35);
-	Fl_Button *studentFileChooserButton = new Fl_Button(350, 50, 55,55, "@+3fileopen");
-	Fl_Button *projectFileChooserButton = new Fl_Button(350,150, 55,55, "@+3fileopen");
-
 	window->begin();
+
+	Fl_Box *labelStudentFile = new Fl_Box(75,50,300,20,"Student File");
 	labelStudentFile->box(FL_FRAME_BOX);
 	labelStudentFile->color(FL_WHITE);
+
+	fileInput_Student = new Fl_Input(75,70,300,35);
+	fileInput_Student->value(".");
+
+	Fl_Box *labelProjectFile = new Fl_Box(75,150,300,20,"Project File");
 	labelProjectFile->box(FL_FRAME_BOX);
 	labelProjectFile->color(FL_WHITE);
 
-	studentFileChooserButton->callback(beepcb, 0);
+	fileInput_Project = new Fl_Input(75,170,300,35);
+	fileInput_Project->value(".");
+
+	Fl_Button *studentFileChooserButton = new Fl_Button(375, 50, 55,55, "@+3fileopen");
+	studentFileChooserButton->callback(chooseStudentFile_cb, 0);
+
+	Fl_Button *projectFileChooserButton = new Fl_Button(375,150, 55,55, "@+3fileopen");
+	projectFileChooserButton->callback(chooseProjectFile_cb, 0);
 
 	window->end();
 	window->show();
--- BruteForce/src/main/FileInputWindow.h
+++ BruteForce/src/main/FileInputWindow.h
@@ -8,14 +8,20 @@
 #ifndef SRC_MAIN_FILEINPUTWINDOW_H_
 #define SRC_MAIN_FILEINPUTWINDOW_H_
 
+#include <stdio.h>
+#include <string.h>
+
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
-#include <FL/Fl_File_Chooser.H>
+#include <FL/Fl_Native_File_Chooser.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Button.H>
 #include <FL/fl_ask.H>
+#include <FL/Fl_Multiline_Input.H>
+
 
 namespace std {
+
 
 class FileInputWindow {
 public:
